ed45fa05c90b3efefa75fa7defc558030330a409
xmieso <jakub17588@gmail.com>
2023-04-04 23:17:52 +0200

    added calc function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    static int calc(int a, int b){
+        return a+b;
+    }
 }
+
